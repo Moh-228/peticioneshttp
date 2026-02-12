e98f2e1efdd4e04db48f9d12102aa093db6a0fc0
Moises <perezzaragoza.josemoises@gmail.com>
2026-02-12 12:07:00 -0600

    Peticion y carga de informacion rick-morti-app-js

--- src/rickmorty/rick-morty-app.js
+++ src/rickmorty/rick-morty-app.js
@@ -1,7 +1,16 @@
 
 
+const fetchQuote = async()=>{
+    const res = await fetch('https://rickandmortyapi.com/api/character/1');
+    const data = await res.json();
+    console.log(data);
+    return data;
+}
 
-export const RickmortyApp = (element)=>{
+
+export const RickmortyApp = async(element)=>{
     document.querySelector('#app-title').innerHTML = 'Rick and Morty App'
     element.innerHTML = 'Loading...'
+    const quote = await fetchQuote();
+    element.innerHTML = 'Tenemos data'
 }
